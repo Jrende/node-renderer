56caa02b254f1bc96247f1b7d6b2cd3af9c0428a
Johan Rende <johan@rende.se>
2017-09-24 21:21:35 +0200

    Renamed stuff

--- src/components/svg/SvgRenderer.jsx
+++ src/components/svg/SvgRenderer.jsx
@@ -9,7 +9,11 @@ class SvgRenderer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dragMode: null,
+      grabFrom: [0, 0],
+      grabTo: [0, 0],
+      grabMode: null,
+      grabNodeId: -1,
+      lastPos: [0, 0]
     }
     this.handleDrop = this.handleDrop.bind(this);
     this.setSvg = this.setSvg.bind(this);
@@ -18,6 +22,7 @@ class SvgRenderer extends React.Component {
     this.onMouseUp = this.onMouseUp.bind(this);
     this.onConnectorMouseUp = this.onConnectorMouseUp.bind(this);
     this.onConnectorMouseDown = this.onConnectorMouseDown.bind(this);
+
   }
 
   componentDidMount() {
@@ -42,51 +47,53 @@ class SvgRenderer extends React.Component {
     event.stopPropagation();
     let c = transformPointToSvgSpace([event.clientX, event.clientY], this.svg);
 
-    let nodeId = -1;
+    let grabNodeId = -1;
+    let grabNodeOutput = null;
     let parent = event.target.parentElement;
     while(!(parent instanceof SVGSVGElement)) {
+      if(parent.hasAttribute("data-output-name")) {
+        grabNodeOutput = parent.getAttribute("data-output-name");
+      }
       if(parent.hasAttribute("data-node-id")) {
-        nodeId = parent.getAttribute("data-node-id");
+        grabNodeId = parent.getAttribute("data-node-id");
         break;
       }
       parent = parent.parentElement;
     }
 
     this.setState({
-      dragMode: "connector",
-      dragNodeId: nodeId,
-      dragTo: c,
-      dragFrom: c,
-      lastX: event.clientX,
-      lastY: event.clientY
+      grabNodeId,
+      grabNodeOutput,
+      grabMode: "connector",
+      grabTo: c,
+      grabFrom: c,
+      lastPos: [event.clientX, event.clientY]
     });
   }
 
   onElementMouseDown(event, node) {
     this.setState({
-      dragMode: "element",
-      dragNodeId: node.id,
-      lastX: event.clientX,
-      lastY: event.clientY
+      grabMode: "element",
+      grabNodeId: node.id,
+      lastPos: [event.clientX, event.clientY]
     })
   }
 
   onMouseMove(event) {
-    if(this.state.dragMode != null) {
-      let dx = event.clientX - this.state.lastX;
-      let dy = event.clientY - this.state.lastY;
+    if(this.state.grabMode != null) {
+      let dx = event.clientX - this.state.lastPos[0];
+      let dy = event.clientY - this.state.lastPos[1];
       this.setState({
-        lastX: event.clientX,
-        lastY: event.clientY
+        lastPos: [event.clientX, event.clientY]
       });
 
-      if(this.state.dragMode === "element") {
-        let node = findNode(this.props.graph, this.state.dragNodeId);
+      if(this.state.grabMode === "element") {
+        let node = findNode(this.props.graph, this.state.grabNodeId);
         let newPos = addInSvgSpace(node.pos, [dx, dy], this.svg)
-        this.props.setNodeLocation(this.state.dragNodeId, newPos);
-      } else if(this.state.dragMode === "connector") {
-        let dragTo = addInSvgSpace(this.state.dragTo, [dx, dy], this.svg)
-        this.setState({ dragTo });
+        this.props.setNodeLocation(this.state.grabNodeId, newPos);
+      } else if(this.state.grabMode === "connector") {
+        let grabTo = addInSvgSpace(this.state.grabTo, [dx, dy], this.svg)
+        this.setState({ grabTo });
       }
     }
   }
@@ -103,15 +110,16 @@ class SvgRenderer extends React.Component {
         }
         parent = parent.parentElement;
       }
-      console.log(`create connection from ${this.state.dragNodeId} to ${nodeId}`);
+      //this.connectNodes(
+      console.log(`create connection from ${this.state.grabNodeId} to ${nodeId}`);
     }
     this.onMouseUp();
   }
 
   onMouseUp(event) {
     this.setState({
-      dragMode: null,
-      dragNodeId: null
+      grabMode: null,
+      grabNodeId: null
     })
   }
 
@@ -126,7 +134,7 @@ class SvgRenderer extends React.Component {
 
   render() {
     let { graph, nodes, connections } = this.props;
-    let { dragTo, dragFrom } = this.state;
+    let { grabTo, grabFrom } = this.state;
     let circles = nodes.map(node => <SvgNode key={node.id} node={node}  onConnectorMouseUp={this.onConnectorMouseUp} onConnectorMouseDown={this.onConnectorMouseDown} onElementMouseDown={(event) => this.onElementMouseDown(event, node)}/>);
     let lines = connections.map(connection => {
       let node1 = nodes[connection[0]];
@@ -135,8 +143,8 @@ class SvgRenderer extends React.Component {
       return <line key={key} x1={node1.pos[0]} y1={node1.pos[1]} x2={node2.pos[0]} y2={node2.pos[1]} strokeWidth="2" stroke="black"/>
     });
     let connectorLine = null;
-    if(this.state.dragMode === "connector") {
-      connectorLine = <line x1={dragFrom[0]} y1={dragFrom[1]} x2={dragTo[0]} y2={dragTo[1]} stroke="black" strokeWidth="2" />
+    if(this.state.grabMode === "connector") {
+      connectorLine = <line x1={grabFrom[0]} y1={grabFrom[1]} x2={grabTo[0]} y2={grabTo[1]} stroke="black" strokeWidth="2" />
     }
     let w = 200;
     let viewBox = `-${w} -${w} ${w*2} ${w*2}`;
