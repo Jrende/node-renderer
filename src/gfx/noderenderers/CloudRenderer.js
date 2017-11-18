cab9d812a8353cf71c1a8dece14f419817c64d9f
Johan Rende <johan@rende.se>
2017-11-18 16:09:13 +0100

    Moved shared renderer code to separate class

--- src/gfx/noderenderers/CloudRenderer.js
+++ src/gfx/noderenderers/CloudRenderer.js
@@ -1,21 +1,11 @@
+import Renderer from './Renderer';
 import shaders from '../shader';
-import Framebuffer from '../Framebuffer.js';
-import VertexArray from '../VertexArray.js';
-export default class CloudRenderer {
+export default class CloudRenderer extends Renderer {
   constructor(gl) {
+    super(gl);
     this.gl = gl;
     this.shader = shaders.cloud;
     this.shader.compile(gl);
-    this.canvas = gl.canvas;
-    this.output = new Framebuffer(this.gl, this.canvas.width, this.canvas.height, false, false);
-    this.quad = new VertexArray(this.gl,
-      [1, 1, 1,
-        -1, 1, 1,
-        -1, -1, 1,
-        1, -1, 1],
-      [1, 0, 2,
-        2, 0, 3],
-      [3]);
   }
 
   /*
@@ -37,11 +27,9 @@ export default class CloudRenderer {
   */
    
   render(values, framebuffers) {
-    //{seed: 0, size: "6", density: 0, left: "5", color: "Black"}
     console.log("values: ", values);
     this.output.renderTo(this.gl, () => {
-      //Might not need to clear depth buffer
-      this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+      this.gl.clear(this.gl.COLOR_BUFFER_BIT);
       this.shader.bind(this.gl);
       this.quad.bind(this.gl);
       this.shader.setUniforms(this.gl, {
