e0c08af7bdff3d7273cd4323781db6a69303971e
Johan Rende <johan@rende.se>
2018-02-07 18:00:21 +0100

    Added saturation/value triangle

--- src/main/resources/frontend/src/components/inputs/ColorInput/ColorInput.jsx
+++ src/main/resources/frontend/src/components/inputs/ColorInput/ColorInput.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import tinycolor from 'tinycolor2';
-import { mat4 } from 'gl-matrix';
+import { mat4, vec2 } from 'gl-matrix';
 import colorWheelVert from './colorWheel.vert';
 import colorWheelFrag from './colorWheel.frag';
 import satValVert from './satVal.vert';
@@ -10,11 +10,30 @@ import VertexArray from '../../../gfx/VertexArray';
 import Shader from '../../../gfx/shader/Shader';
 import './style.less';
 
+function getAllOffsetLeft(elm) {
+  let offsetLeft = 0;
+  while(elm !== null) {
+    offsetLeft += elm.offsetLeft;
+    elm = elm.offsetParent;
+  }
+  return offsetLeft;
+}
+
+function getAllOffsetTop(elm) {
+  let offsetTop = 0;
+  while(elm !== null) {
+    offsetTop += elm.offsetTop;
+    elm = elm.offsetParent;
+  }
+  return offsetTop;
+}
+
 class ColorInput extends React.Component {
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
     this.setCanvas = this.setCanvas.bind(this);
+    this.onCanvasClick = this.onCanvasClick.bind(this);
   }
 
   componentDidMount() {
@@ -27,17 +46,27 @@ class ColorInput extends React.Component {
     this.satValShader = new Shader({ frag: satValFrag, vert: satValVert });
     this.satValShader.compile(this.gl);
     this.quad = new VertexArray(this.gl,
-      [1, 1,
+      [
+        1, 1,
         -1, 1,
         -1, -1,
-        1, -1],
+        1, -1
+      ],
       [1, 0, 2,
         2, 0, 3],
       [2]);
+    let t = [
+      [0, 1.0],
+      [-0.8660253882408142, -0.5],
+      [0.8660253882408142, -0.5]
+    ];
+    this.triP = t;
     this.triangle = new VertexArray(this.gl,
-      [0, 1.0, 1.0, 1.0,
-        -0.8660253882408142, -0.5, 0.0, 1.0,
-        0.8660253882408142, -0.4999999701976776, 1.0, 0.0],
+      [
+        t[0][0], t[0][1], 1.0, 1.0,
+        t[1][0], t[1][1], 1.0, 0.0,
+        t[2][0], t[2][1], 0.0, 1.0
+      ],
       [0, 1, 2],
       [2, 2]);
     this.triangleModel = mat4.fromScaling(mat4.create(), [0.8, 0.8, 1.0]);
@@ -52,6 +81,26 @@ class ColorInput extends React.Component {
     this.props.onChange(color.toRgb());
   }
 
+  onCanvasClick(event) {
+    let coords = [
+      (event.pageX - getAllOffsetLeft(this.canvas)) / this.canvas.offsetWidth,
+      (event.pageY - getAllOffsetTop(this.canvas)) / this.canvas.offsetHeight
+    ];
+    /*
+    if(this.clickedOnWheel(coords)) {
+      console.log('clicked on color wheel');
+    }
+    */
+    if (this.clickedOnTriangle([coords[0], coords[1] - 0.5])) {
+      console.log('clicked on triangle');
+    }
+  }
+
+  clickedOnWheel(p) {
+    let dist = vec2.distance(p, [0.5, 0.5]);
+    return (dist > 0.4 && dist < 0.5);
+  }
+
   setCanvas(canvas) {
     this.canvas = canvas;
   }
@@ -60,11 +109,10 @@ class ColorInput extends React.Component {
     this.gl.clear(this.gl.COLOR_BUFFER_BIT);
     this.quad.bind(this.gl);
 
-
     let resolution = [
       this.canvas.clientWidth,
       this.canvas.clientHeight
-    ]
+    ];
     this.colorWheelShader.bind(this.gl);
     this.colorWheelShader.setUniforms(this.gl, {
       resolution
@@ -90,14 +138,19 @@ class ColorInput extends React.Component {
   }
 
   render() {
-    let { name, type, value } = this.props;
-    let color = tinycolor(value);
+    let { name, value } = this.props;
     if(this.gl !== undefined) {
-      this.renderCanvas(color);
+      this.renderCanvas(value);
     }
     return (
       <fieldset>
-        <canvas width="256" height="256" className="color-input-canvas" ref={this.setCanvas} />
+        <canvas
+          onClick={this.onCanvasClick}
+          width="256"
+          height="256"
+          className="color-input-canvas"
+          ref={this.setCanvas}
+        />
       </fieldset>
     );
   }
@@ -105,7 +158,6 @@ class ColorInput extends React.Component {
 
 ColorInput.propTypes = {
   name: PropTypes.string.isRequired,
-  type: PropTypes.object.isRequired,
   value: PropTypes.object,
   onChange: PropTypes.func
 };
